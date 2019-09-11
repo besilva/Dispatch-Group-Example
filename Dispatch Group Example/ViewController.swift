5496ffeffbeb0b9b100ebe8faa5f31be58e9a756
Bernardo Silva <silva98.bernardo@gmail.com>
2019-09-11 10:30:25 -0300

    Initial Commit

---
+++ Dispatch Group Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dispatch Group Example
+//
+//  Created by Bernardo Silva on 11/09/19.
+//  Copyright © 2019 Bernardo Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
